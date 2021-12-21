c76224d6a6e9a12a82daec51a382b04325263365
Shirigume <aleks_xr@abv.bg>
2021-12-21 21:57:47 +0200

    d

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ let btn = document.getElementById('btn');
 btn.addEventListener('click', function(){
 let weight = document.getElementById('weight-imput'). value;
 let weight = document.getElementById('height-imput'). value;
-let finalbmi = (weight / height * height) * 10000;
+let finalbmi = (weight / height * height) * 10000);
 document.getElementById('bmi-output').value = finalbmi;
 });
