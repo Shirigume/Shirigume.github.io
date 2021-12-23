de9175c100a41c0f103a36c9922ecd54b1f21060
Shirigume <aleks_xr@abv.bg>
2021-12-23 18:55:18 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,21 @@
-let btn = document.getElementById('btn');
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const calculate = document.querySelector("#calculate");
+const yourBMI = document.querySelector("#yourBMI");
 
-btn.addEventListener('click', function(){
-   let weight = document.getElementById('weight-input').value;
-   let height = document.getElementById('height-input').value / 100;
-   let finalBmi = weight / (height * height);
-    document.getElementById('bmi-output').value = finalBmi;
+calculate.addEventListener("click" , ()=>{
+    //BMI = Body Mass Index
+    //m = Mass Means Weight (KG)
+    //h = Height (CM)
+    //Formula = B = m/h²
+    
+    
+    if(height.value != "" && weight.value != ""){
+        let bmiValue = weight.value / (height.value * height.value) * 10000;
+        yourBMI.innerHTML = `Your BMI Is : <span> ${bmiValue.toFixed(2)} </span>`
+    }
+    else
+        {
+            yourBMI.innerHTML = `Please Enter Correct Value`;
+        }
 });
